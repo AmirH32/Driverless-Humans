48896f1f86817b56ec707f90d43df72a1b638be5
AmirH32 <amirhassanali2610@gmail.com>
2025-02-18 12:09:01 +0000

    Removed unecessary comment

--- Backend/data/models.py
+++ Backend/data/models.py
@@ -3,8 +3,6 @@ from pydantic import BaseModel, Field
 from typing import Literal
 from flask_sqlalchemy import SQLAlchemy
 
-
-# Needs fixing because we are using SQL alchemy you can write python class like objects that map directly to tables in the DB
 class Timetable(BaseModel):
     route_id    : int
     route_name  : str
@@ -29,7 +27,7 @@ class AccessibilityRequirement(db.Model):
     Description = db.Column(db.String(255), unique=False, nullable=True)  # Optional description for the requirement
 
 class UserAccessibility(db.Model):
-    __tablename__ = 'user_accessibility'
+    __tablename__ = 'User_Accessibility'
     # Cascade delete so if we delete a user or an accessibility all associated records are automatically deleted
     UserID = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), primary_key=True, unique=True)
     RequirementID = db.Column(db.Integer, db.ForeignKey('accessibility_requirement.id', ondelete='CASCADE'), primary_key=True, unique=True)
