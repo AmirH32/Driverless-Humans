18663079d1534d9ec1c2b4736024c6d31ae985ff
Alice Sharam <as3520@cam.ac.uk>
2025-02-21 23:56:34 +0000

    Various bug fixes to get docker working

--- Backend/init-scripts/init.sql
+++
@@ -1,9 +0,0 @@
--- If mydatabase doesn't exist create it
-DO
-$$
-BEGIN
-   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'mydatabase') THEN
-      CREATE DATABASE mydatabase;
-   END IF;
-END
-$$;
