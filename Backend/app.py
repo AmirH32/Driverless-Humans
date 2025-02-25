2845cf9b5691edfe1c107689e3c5f197cf2eeb13
AmirH32 <amirhassanali2610@gmail.com>
2025-02-25 11:21:42 +0000

    Formatted our code to industry standard

--- Backend/app.py
+++ Backend/app.py
@@ -8,10 +8,24 @@ from .data.stops import get_autocomplete_stops
 
 from flask import Flask, request, jsonify, make_response
 
-from .database.models import db, User, Reservations, UserReservation, AccessibilityRequirement, AccessibilityOptions, UserAccessibility, UserToOptions
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, create_refresh_token
-from .authorisation.auth import init_jwt, create_user, is_strong_password, is_valid_email
-from flask_sqlalchemy import SQLAlchemy
+from .database.models import (
+    db,
+    User,
+    Reservations,
+    UserReservation,
+)
+from flask_jwt_extended import (
+    create_access_token,
+    jwt_required,
+    get_jwt_identity,
+    create_refresh_token,
+)
+from .authorisation.auth import (
+    init_jwt,
+    create_user,
+    is_strong_password,
+    is_valid_email,
+)
 from markupsafe import escape
 
 from flask_talisman import Talisman
@@ -24,28 +38,24 @@ load_dotenv()
 app = Flask(__name__)
 
 ### Only need this for development on browser but should work without on phones
-CORS(app, supports_credentials=True, resources={r"/*": {"origins": ["http://localhost:8081", "http://127.0.0.1:8081"]}})
+CORS(
+    app,
+    supports_credentials=True,
+    resources={r"/*": {"origins": ["http://localhost:8081", "http://127.0.0.1:8081"]}},
+)
 
 
 # Define Content Security Policy (CSP) to allow frontend
 csp = {
-    'default-src': [
+    "default-src": [
         "'self'",  # Allow content from the same origin
         "http://localhost:8081",  # Allow frontend origin
     ],
-    'script-src': [
+    "script-src": ["'self'", "'unsafe-inline'", "http://localhost:8081"],
+    "style-src": ["'self'", "'unsafe-inline'", "http://localhost:8081"],
+    "img-src": [
         "'self'",
-        "'unsafe-inline'",  
-        "http://localhost:8081"
-    ],
-    'style-src': [
-        "'self'",
-        "'unsafe-inline'",
-        "http://localhost:8081"
-    ],
-    'img-src': [
-        "'self'",
-        "data:"  # Allow inline images
+        "data:",  # Allow inline images
     ],
 }
 ###
@@ -60,14 +70,18 @@ else:
 def sanitise_input(string):
     return escape(string.strip())
 
+
 def add_and_commit(entry):
     db.session.add(entry)
     db.session.commit()
 
+
 def auth_init():
     # This connects to local postgresql docker instance, change in future if you want it on a public server
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://myuser:mypassword@localhost:5432/mydatabase'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config["SQLALCHEMY_DATABASE_URI"] = (
+        "postgresql://myuser:mypassword@localhost:5432/mydatabase"
+    )
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     # Initialise the database with the Flask app
     db.init_app(app)
@@ -81,7 +95,8 @@ def auth_init():
 
 auth_init()
 
-@app.route('/timetables', methods=['GET'])
+
+@app.route("/timetables", methods=["GET"])
 @jwt_required()
 def timetables() -> List[Dict[str, Any]]:
     """
@@ -91,12 +106,13 @@ def timetables() -> List[Dict[str, Any]]:
 
     Example (with William Gates Building): http://127.0.0.1:5000/timetables?stop_id=0500CCITY424
     """
-    stop_id = request.args.get('stop_id')
+    stop_id = request.args.get("stop_id")
     timetables = get_timetables(stop_id)
-    timetables_json = [timetable.model_dump(mode='json') for timetable in timetables]
+    timetables_json = [timetable.model_dump(mode="json") for timetable in timetables]
     return timetables_json
 
-@app.route('/autocomplete', methods=['GET'])
+
+@app.route("/autocomplete", methods=["GET"])
 @jwt_required()
 def autocomplete() -> List[Dict[str, Any]]:
     """
@@ -104,69 +120,91 @@ def autocomplete() -> List[Dict[str, Any]]:
 
     :returns autocomplete_json: A list of dictionaries containing autocompletion suggestions in the format of `data.models.Autocompletion`
     """
-    input = request.args.get('input')
-    limit = int(request.args.get('limit'))
+    input = request.args.get("input")
+    limit = int(request.args.get("limit"))
 
     autocompletions = get_autocomplete_stops(name=input, limit=limit)
-    autocompletions_json = [autocompletion.model_dump(mode='json') for autocompletion in autocompletions]
+    autocompletions_json = [
+        autocompletion.model_dump(mode="json") for autocompletion in autocompletions
+    ]
     return autocompletions_json
 
-@app.route('/login', methods=['POST'])
+
+@app.route("/login", methods=["POST"])
 def login():
     data = request.get_json()
-    email = sanitise_input(data.get('email'))
-    password = data.get('password')
+    email = sanitise_input(data.get("email"))
+    password = data.get("password")
 
     user = User.query.filter_by(Email=email).first()
     if user and user.verify_password(password):
-        access_token = create_access_token(identity=str(user.UserID), expires_delta = timedelta(hours=1))
-        refresh_token = create_refresh_token(identity=str(user.UserID), expires_delta=timedelta(days=7))  # 7 days expiry
-        
+        access_token = create_access_token(
+            identity=str(user.UserID), expires_delta=timedelta(hours=1)
+        )
+        refresh_token = create_refresh_token(
+            identity=str(user.UserID), expires_delta=timedelta(days=7)
+        )  # 7 days expiry
+
         # Set JWT in HttpOnly cookie
-        response = make_response(jsonify({'message': 'Login successful', 'success': True}))
+        response = make_response(
+            jsonify({"message": "Login successful", "success": True})
+        )
         # Change samesite to "Strict" in prod
-        response.set_cookie('access_token', access_token, httponly=True, secure=True, samesite='None')
-        response.set_cookie('refresh_token', refresh_token, httponly=True, secure=True, samesite='None')
+        response.set_cookie(
+            "access_token", access_token, httponly=True, secure=True, samesite="None"
+        )
+        response.set_cookie(
+            "refresh_token", refresh_token, httponly=True, secure=True, samesite="None"
+        )
 
         return response, 200
     else:
-        return jsonify({'message': 'Invalid email or password', 'success': False}), 401
+        return jsonify({"message": "Invalid email or password", "success": False}), 401
 
-@app.route('/register', methods=['POST'])
+
+@app.route("/register", methods=["POST"])
 def register():
     data = request.get_json()
-    email = sanitise_input(data.get('email', ''))
-    name = sanitise_input(data.get('name', ''))
-    password = data.get('password', '')
+    email = sanitise_input(data.get("email", ""))
+    name = sanitise_input(data.get("name", ""))
+    password = data.get("password", "")
 
     # Make sure email and password fields are filled
     if not email or not password or not name:
-        return jsonify({'message': 'All fields are required', 'success': False}), 400
+        return jsonify({"message": "All fields are required", "success": False}), 400
 
     if not is_valid_email(email):
-        return jsonify({'message': 'Invalid email format', 'success': False}), 400
-    
+        return jsonify({"message": "Invalid email format", "success": False}), 400
+
     if not is_strong_password(password):
-        return jsonify({'message': 'Password must be at least 16 characters long, include an uppercase letter, a lowercase letter, a digit, and a special character.', 'success': False}), 400
+        return jsonify(
+            {
+                "message": "Password must be at least 16 characters long, include an uppercase letter, a lowercase letter, a digit, and a special character.",
+                "success": False,
+            }
+        ), 400
 
     # Check if user already exists
     existing_user = User.query.filter_by(Email=email).first()
     if existing_user:
-        return jsonify({'message': 'User already exists', 'success': False}), 400
+        return jsonify({"message": "User already exists", "success": False}), 400
 
     # Create the user
     create_user(email, password, name)
-    
-    return jsonify({'message': 'User created successfully', 'success': True}), 201
 
-@app.route('/logout', methods=['POST'])
+    return jsonify({"message": "User created successfully", "success": True}), 201
+
+
+@app.route("/logout", methods=["POST"])
 @jwt_required()
 def logout():
-    response = make_response(jsonify({'message': 'Logout successful', 'success': True}), 200)
+    response = make_response(
+        jsonify({"message": "Logout successful", "success": True}), 200
+    )
 
     # Clear access and refresh tokens from cookies
-    response.set_cookie('access_token', '', expires=0)
-    response.set_cookie('refresh_token', '', expires=0)
+    response.set_cookie("access_token", "", expires=0)
+    response.set_cookie("refresh_token", "", expires=0)
 
     return response
 
@@ -175,86 +213,125 @@ def logout():
 @app.route("/refresh")
 @jwt_required(refresh=True)
 def refresh(self):
-    try: 
+    try:
         current_user = get_jwt_identity()
-        new_access_token = create_access_token(identity=str(current_user), fresh=False, expires_delta=timedelta(hours=1))
-        new_refresh_token = create_refresh_token(identity=str(current_user), expires_delta=timedelta(days=7)) 
+        new_access_token = create_access_token(
+            identity=str(current_user), fresh=False, expires_delta=timedelta(hours=1)
+        )
+        new_refresh_token = create_refresh_token(
+            identity=str(current_user), expires_delta=timedelta(days=7)
+        )
 
-
-        response = make_response(jsonify({'access_token': new_access_token}), 200)
+        response = make_response(jsonify({"access_token": new_access_token}), 200)
         # CHANGE samesite to "Strict" in prod
-        response.set_cookie('access_token', new_access_token, httponly=True, secure=True, samesite='None')
+        response.set_cookie(
+            "access_token",
+            new_access_token,
+            httponly=True,
+            secure=True,
+            samesite="None",
+        )
         # To prevent logging the user out after 7 days, everytime the user sends a request to refresh, their refresh token is renewed
-        response.set_cookie('refresh_token', new_refresh_token, httponly=True, secure=True, samesite='None')
+        response.set_cookie(
+            "refresh_token",
+            new_refresh_token,
+            httponly=True,
+            secure=True,
+            samesite="None",
+        )
         return response
-    except: 
+    except:
         # Refresh token has expired
-        return jsonify({'message': 'Please login again.'}), 401
+        return jsonify({"message": "Please login again."}), 401
 
-@app.route('/create_reservation', methods=['POST'])
-@jwt_required() # Protect this route
+
+@app.route("/create_reservation", methods=["POST"])
+@jwt_required()  # Protect this route
 def create_reservation():
     data = request.get_json()
-    stopID1 = sanitise_input(data.get('StopID1',''))
-    stopID2 = sanitise_input(data.get('StopID2',''))
-    busID = sanitise_input(data.get('BusID',''))
-    time = data.get('Time', '')
+    stopID1 = sanitise_input(data.get("StopID1", ""))
+    stopID2 = sanitise_input(data.get("StopID2", ""))
+    busID = sanitise_input(data.get("BusID", ""))
+    time = data.get("Time", "")
 
     userID = get_jwt_identity()
 
     if not all([stopID1, stopID2, busID, time]):
-        return jsonify({'message': 'Missing required fields'}), 400
-    
+        return jsonify({"message": "Missing required fields"}), 400
+
     # Check if reservation exists
-    existing_user_reservations = db.session.query(UserReservation).filter_by(UserID=userID).all()
+    existing_user_reservations = (
+        db.session.query(UserReservation).filter_by(UserID=userID).all()
+    )
     if existing_user_reservations:
         for user_reservation in existing_user_reservations:
             reservation_id = user_reservation.ReservationID
-            reservation = db.session.query(Reservations).filter_by(ReservationID=reservation_id, Time=time).first()
-            if reservation: 
-                  return jsonify({'message': 'This reservation already exists for this user.'}), 400
+            reservation = (
+                db.session.query(Reservations)
+                .filter_by(ReservationID=reservation_id, Time=time)
+                .first()
+            )
+            if reservation:
+                return jsonify(
+                    {"message": "This reservation already exists for this user."}
+                ), 400
             else:
                 pass
-    try: 
+    try:
         new_reservation = Reservations(
             StopID1=stopID1,
             StopID2=stopID2,
             BusID=busID,
-            Time=datetime.strptime(time, "%Y-%m-%d %H:%M:%S")
+            Time=datetime.strptime(time, "%Y-%m-%d %H:%M:%S"),
         )
 
         add_and_commit(new_reservation)
 
         # Link the user to the reservation
-        user_reservation = UserReservation(UserID=userID, ReservationID=new_reservation.ReservationID)
+        user_reservation = UserReservation(
+            UserID=userID, ReservationID=new_reservation.ReservationID
+        )
         add_and_commit(user_reservation)
 
-        return jsonify({'message': 'Reservation created successfully', 'reservation_id': new_reservation.ReservationID}), 201
+        return jsonify(
+            {
+                "message": "Reservation created successfully",
+                "reservation_id": new_reservation.ReservationID,
+            }
+        ), 201
 
     except Exception as e:
         db.session.rollback()  # Rollback in case of error
-        return jsonify({'error': str(e)}), 500
+        return jsonify({"error": str(e)}), 500
 
-@app.route('/delete_reservation', methods=['POST'])
+
+@app.route("/delete_reservation", methods=["POST"])
 @jwt_required()
 def delete_reservation():
     try:
         userID = get_jwt_identity()
 
         # Find the most recent reservation for the user to delete
-        reservation = db.session.query(Reservations).join(UserReservation).filter(UserReservation.UserID == userID).order_by(Reservations.Time.desc()).first()
+        reservation = (
+            db.session.query(Reservations)
+            .join(UserReservation)
+            .filter(UserReservation.UserID == userID)
+            .order_by(Reservations.Time.desc())
+            .first()
+        )
 
         if not reservation:
-            return jsonify({'message': 'No reservations found for this user.'}), 404
+            return jsonify({"message": "No reservations found for this user."}), 404
 
         # Delete the reservation
         db.session.delete(reservation)
         db.session.commit()
 
-        return jsonify({'message': 'Reservation deleted successfully.'}), 200
+        return jsonify({"message": "Reservation deleted successfully."}), 200
     except Exception as e:
         db.session.rollback()
-        return jsonify({'error': str(e)}), 500
+        return jsonify({"error": str(e)}), 500
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
--- Backend/data/stops.py
+++ Backend/data/stops.py
@@ -7,57 +7,80 @@ from rapidfuzz import fuzz
 from .utils import gridreference_to_latlong
 from .models import Autocompletion
 
-STOPS_DATA_FILENAME = 'data/stops.csv'
+STOPS_DATA_FILENAME = "data/stops.csv"
+
 
 def fetch_stops_data():
-	"""
-	Fetches bus stop data from the NaPTAN API and saves it in a CSV file.
+    """
+    Fetches bus stop data from the NaPTAN API and saves it in a CSV file.
 
-	@param filename: str, where to save the CSV
+    @param filename: str, where to save the CSV
 
-	@return: DataFrame, bus data in JSON format
-	"""
-	url = 'https://naptan.api.dft.gov.uk/v1/access-nodes/'
-	params = {
-		'atcoAreaCodes': '050', # Cambridge
-		'dataFormat': 'csv'
-	}
-	response = requests.get(url, params=params)
+    @return: DataFrame, bus data in JSON format
+    """
+    url = "https://naptan.api.dft.gov.uk/v1/access-nodes/"
+    params = {
+        "atcoAreaCodes": "050",  # Cambridge
+        "dataFormat": "csv",
+    }
+    response = requests.get(url, params=params)
 
-	if response.status_code == 200:
-		return pd.read_csv(StringIO(response.text))
-	else:
-		print(f"Error: {response.status_code} - {response.text}")
-		return None
+    if response.status_code == 200:
+        return pd.read_csv(StringIO(response.text))
+    else:
+        print(f"Error: {response.status_code} - {response.text}")
+        return None
+
 
 def save_stops_data(filename=STOPS_DATA_FILENAME):
-	df = fetch_stops_data()
-	assert (df.GridType.unique()=='UKOS').all()
+    df = fetch_stops_data()
+    assert (df.GridType.unique() == "UKOS").all()
 
-	df[['Latitude', 'Longitude']] = df.apply(lambda row:gridreference_to_latlong(row['Easting'],row['Northing']),axis=1,result_type='expand')
-	df = df[['ATCOCode','CommonName', 'Latitude', 'Longitude', 'Street']]
-	df = df.rename(columns={'ATCOCode':'id','CommonName':'name', 'Latitude':'latitude', 'Longitude':'longitude', 'Street':'street'})
+    df[["Latitude", "Longitude"]] = df.apply(
+        lambda row: gridreference_to_latlong(row["Easting"], row["Northing"]),
+        axis=1,
+        result_type="expand",
+    )
+    df = df[["ATCOCode", "CommonName", "Latitude", "Longitude", "Street"]]
+    df = df.rename(
+        columns={
+            "ATCOCode": "id",
+            "CommonName": "name",
+            "Latitude": "latitude",
+            "Longitude": "longitude",
+            "Street": "street",
+        }
+    )
 
-	df['street'] = df['street'].fillna(df['name'])
+    df["street"] = df["street"].fillna(df["name"])
 
-	df.to_csv(filename,index=False)
-	return df
+    df.to_csv(filename, index=False)
+    return df
+
 
 def get_stops_data(filename=STOPS_DATA_FILENAME):
-	if not os.path.exists(filename):
-		save_stops_data(filename)
-	df = pd.read_csv(filename,index_col='id')
-	return df
+    if not os.path.exists(filename):
+        save_stops_data(filename)
+    df = pd.read_csv(filename, index_col="id")
+    return df
+
 
 def get_autocomplete_stops(name: str, limit: int) -> list[Autocompletion]:
     name = name.lower()
-    
+
     stops = get_stops_data()
-    stops['prefix'] = stops['name'].str[:len(name)].str.lower()
-    stops['similarity'] = stops.apply(lambda row: fuzz.ratio(row['prefix'], name), axis=1)
-    stops.sort_values(by='similarity', ascending=False, inplace=True)
-    stops_dict = stops.reset_index()[['id','name','street']].head(limit).to_dict(orient='records')
+    stops["prefix"] = stops["name"].str[: len(name)].str.lower()
+    stops["similarity"] = stops.apply(
+        lambda row: fuzz.ratio(row["prefix"], name), axis=1
+    )
+    stops.sort_values(by="similarity", ascending=False, inplace=True)
+    stops_dict = (
+        stops.reset_index()[["id", "name", "street"]]
+        .head(limit)
+        .to_dict(orient="records")
+    )
     return [Autocompletion(**stop) for stop in stops_dict]
 
-if __name__ == '__main__':
-	save_stops_data()
+
+if __name__ == "__main__":
+    save_stops_data()
