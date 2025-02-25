9883052606e45c02cea7c6d7fb61a43e85712433
Leong Eu-Shaun <83213769+eutroph@users.noreply.github.com>
2025-02-25 15:16:49 +0000

    Added origin and destination search bar

--- Frontend/app/(tabs)/search.tsx
+++ Frontend/app/(tabs)/search.tsx
@@ -1,15 +1,7 @@
-// With reference from https://www.youtube.com/watch?v=Dp8cQU2OcFU
-// https://github.com/JeremyPersing/airbnbtut/blob/master/screens/AutocompleteScreen.tsx
-
 import React, { useState } from "react";
-import {
-  Text,
-  StyleSheet,
-  TextInput,
-  FlatList,
-  Pressable,
-  View,
-} from "react-native";
+import { Text, View, StyleSheet, Button, Alert } from "react-native";
+import AutocompleteInput from "@/components/AutocompleteSearchBar";
+// import CookieManager from '@react-native-cookies/cookies';
 
 type Stop = {
   id: string;
@@ -18,50 +10,40 @@ type Stop = {
 };
 
 export default function SearchScreen() {
-  const [input, setInput] = useState<string>("");
-  const [data, setData] = useState<Stop[]>([]);
+  const [origin, setOrigin] = useState<Stop | null>(null);
+  const [destination, setDestination] = useState<Stop | null>(null);
 
-  const onSelect = async (item: Stop) => {
-    alert("option chosen: " + JSON.stringify(item));
-  };
-
-  const onChangeText = async (text: string) => {
-    setInput(text);
-    if (text.length > 2) {
-        let response = await fetch(`http://127.0.0.1:5000/autocomplete?input=${text}&limit=${5}`);
-        if (response) {
-            let data: Stop[] = await response.json();
-            if (data.length > 0) {
-                setData(data);
-            }
-        }
+  const handleSubmit = () => {
+    if (origin && destination) {
+      alert("Navigating to Schedule from " + origin.id + " to " + destination.id);
     }
   };
 
+  // const cookies = CookieManager.get("http://127.0.0.1:5000");
+  // console.log(cookies);
+
   return (
     <View style={styles.container}>
-        <Text style={styles.searchLabel}>Start your bus journey here:</Text>
-        <TextInput onChangeText={onChangeText} value={input} style={styles.input} placeholder="Bus stop name"/>
-        {input && data.length > 0 ? (
-        <FlatList
-            data={data}
-            showsVerticalScrollIndicator={true}
-            renderItem={({ item }) => (
-            <Pressable
-                style={({ pressed }) => [{ opacity: pressed ? 0.5 : 1 }]}
-                onPress={ () => onSelect(item) }
-            >
-                <View style={styles.itemContainer}>
-                    <View style={styles.itemTextContainer}>
-                    <Text style={styles.itemTitle}>{item.name}</Text>
-                    <Text style={styles.itemSubtitle}>{item.street}, {item.id}</Text>
-                    </View>
-                </View>
-            </Pressable>
-            )}
-            keyExtractor={(item, index) => index.toString()}
-        />
-        ) : null}
+      <AutocompleteInput label="Origin" onSelect={setOrigin} />
+      <AutocompleteInput label="Destination" onSelect={setDestination} />
+
+      {origin && (
+        <View>
+          <Text>Origin: {origin.name}</Text>
+          <Text>Street: {origin.street}</Text>
+        </View>
+      )}
+
+      {destination && (
+        <View>
+          <Text>Destination: {destination.name}</Text>
+          <Text>Street: {destination.street}</Text>
+        </View>
+      )}
+      
+      <View style={styles.buttonContainer}>
+        <Button onPress={handleSubmit} title="Submit" />
+      </View>
     </View>
   );
 }
@@ -69,34 +51,10 @@ export default function SearchScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
+    padding: 12,
   },
-  searchLabel: {
-    marginLeft: 12,
-    marginVertical: 5,
-    fontSize: 12,
-  },
-  input: {
-    height: 40,
-    marginHorizontal: 12,
-    borderWidth: 1,
-    paddingHorizontal: 10,
-    borderRadius: 5,
-    borderColor: 'gray',
-  },
-  itemContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    padding: 15,
-  },
-  itemTextContainer: {
-    marginLeft: 10,
-    flexShrink: 1,
-  },
-  itemTitle: {
-    fontWeight: "700",
-  },
-  itemSubtitle: {
-    fontSize: 12,
+  buttonContainer: {
+    marginTop: 20,
   },
 });
