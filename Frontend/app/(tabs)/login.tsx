ab29da7636df6033ee497a13c6d346559150f07e
AmirH32 <amirhassanali2610@gmail.com>
2025-02-25 11:48:55 +0000

    Readded login logic that someone removed

--- Frontend/app/(tabs)/login.tsx
+++ Frontend/app/(tabs)/login.tsx
@@ -4,14 +4,11 @@ import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
 export default function LoginScreen() {
-  const handlePress = (buttonType: string) => {
-    console.log(`${buttonType} Pressed!`);
-  };
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleLogin = async () => {
+  const handlePress = async () => {
     try {
       const requestBody = { email, password };
 
