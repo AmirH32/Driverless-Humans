7177678ebc3ed052a04de549c4ba40658f5c8862
Leong Eu-Shaun <83213769+eutroph@users.noreply.github.com>
2025-03-09 17:29:11 +0000

    Confirmed (#14)
    * Added logic for see_reservation backend
    
    * Updated confirmation page for users

--- Backend/database/models.py
+++ Backend/database/models.py
@@ -1,9 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import check_password_hash
+from enum import Enum
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
+
+class Roles(Enum):
+    VOLUNTEER = "Volunteer"
+    DISABLED = "Disabled"
+
 
 class Document(db.Model):
     __tablename__ = 'document'
