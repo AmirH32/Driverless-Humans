fd11cb10253b9ec6b0ae5435c52521e6bf04d216
Alice Sharam <as3520@cam.ac.uk>
2025-03-09 22:40:25 +0000

    Returned index.tsx to how it was before

--- Frontend/app/(tabs)/index.tsx
+++ Frontend/app/(tabs)/index.tsx
@@ -1,6 +1,5 @@
 import { Redirect } from 'expo-router';
-import { FontSizeProvider } from '@/contexts/FontSizeContext'
 
 export default function Index() {
-  <Redirect href="/home" />
+  return <Redirect href="/home" />;
 }
