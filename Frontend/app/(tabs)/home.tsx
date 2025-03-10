9f27041d4682d7e364ab01021cf1b2176f4e9b31
Alice Sharam <as3520@cam.ac.uk>
2025-03-10 13:09:07 +0000

    Added font scaling to home

--- Frontend/app/(tabs)/home.tsx
+++ Frontend/app/(tabs)/home.tsx
@@ -3,9 +3,14 @@ import { TouchableOpacity, StyleSheet, Text, Image } from 'react-native';
 import { useRouter } from 'expo-router';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { useFontSize } from '@/contexts/FontSizeContext';
 
 export default function HomeScreen() {
   const router = useRouter();
+
+  // Font scaling
+  const {fontScale, setFontScale} = useFontSize();
+  const styles = createStyles(fontScale);
 
   return (
     <ThemedView style={styles.container}>
@@ -41,56 +46,61 @@ export default function HomeScreen() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingHorizontal: 20,
-    marginTop: -40,
-  },
-  topLeftImage: {
-    position: 'absolute',
-    top: 65,
-    left: 0,
-    width: 200,
-    height: 100,
-    resizeMode: 'contain',
-  },
-  mainImage: {
-    width: 390,
-    height: 280,
-    marginBottom: 0,
-    resizeMode: 'contain',
-  },
-  title: {
-    fontSize: 32,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    lineHeight: 40,
-    letterSpacing: 2,
-    marginBottom: 30,
-  },
-  button: {
-    backgroundColor: '#007BFF',
-    paddingVertical: 12,
-    borderRadius: 15,
-    marginTop: 20,
-  },
-  buttonText: {
-    color: 'white',
-    fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  loginButton: {
-    paddingHorizontal: 100,
-  },
-  signupButton: {
-    paddingHorizontal: 90,
-  },
-  volunteerButton: {
-    backgroundColor: '#8400BD',
-    paddingHorizontal: 23,
-  },
-});
+const createStyles = (fontScale:number) => {
+  return (
+    StyleSheet.create({
+      container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingHorizontal: 20,
+        marginTop: -40,
+      },
+      topLeftImage: {
+        position: 'absolute',
+        top: 65,
+        left: 0,
+        width: 200,
+        height: 100,
+        resizeMode: 'contain',
+      },
+      mainImage: {
+        width: 390,
+        height: 280,
+        marginBottom: 0,
+        resizeMode: 'contain',
+      },
+      title: {
+        fontSize: 32 * fontScale,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        lineHeight: 40 * fontScale,
+        letterSpacing: 2,
+        marginBottom: 30,
+      },
+      button: {
+        backgroundColor: '#007BFF',
+        paddingVertical: 12,
+        borderRadius: 15,
+        marginTop: 20,
+      },
+      buttonText: {
+        color: 'white',
+        fontSize: 20 * fontScale,
+        lineHeight: 20 * fontScale * 1.2,
+        fontWeight: 'bold',
+        textAlign: 'center',
+      },
+      loginButton: {
+        paddingHorizontal: 100,
+      },
+      signupButton: {
+        paddingHorizontal: 90,
+      },
+      volunteerButton: {
+        backgroundColor: '#8400BD',
+        paddingHorizontal: 23,
+      },
+    })
+  )
+};
