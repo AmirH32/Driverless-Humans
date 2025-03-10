9d958dcf9a69b7add6797366a7011eb43c79d992
Alice Sharam <as3520@cam.ac.uk>
2025-03-10 12:39:33 +0000

    Added font scaling to ThemedText.tsx

--- Frontend/components/ThemedText.tsx
+++ Frontend/components/ThemedText.tsx
@@ -1,6 +1,7 @@
 import { Text, type TextProps, StyleSheet } from 'react-native';
 
 import { useThemeColor } from '@/hooks/useThemeColor';
+import { useFontSize } from '@/contexts/FontSizeContext';
 
 export type ThemedTextProps = TextProps & {
   lightColor?: string;
@@ -16,6 +17,8 @@ export function ThemedText({
   ...rest
 }: ThemedTextProps) {
   const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
+  const {fontScale, setFontScale} = useFontSize();
+  const styles = createStyles(fontScale);
 
   return (
     <Text
@@ -33,28 +36,33 @@ export function ThemedText({
   );
 }
 
-const styles = StyleSheet.create({
-  default: {
-    fontSize: 16,
-    lineHeight: 24,
-  },
-  defaultSemiBold: {
-    fontSize: 16,
-    lineHeight: 24,
-    fontWeight: '600',
-  },
-  title: {
-    fontSize: 32,
-    fontWeight: 'bold',
-    lineHeight: 32,
-  },
-  subtitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  link: {
-    lineHeight: 30,
-    fontSize: 16,
-    color: '#0a7ea4',
-  },
-});
+const createStyles = (fontScale:number) =>{
+  return(
+    StyleSheet.create({
+      default: {
+        fontSize: 16 * fontScale,
+        lineHeight: 24 * fontScale,
+      },
+      defaultSemiBold: {
+        fontSize: 16 * fontScale,
+        lineHeight: 24 * fontScale,
+        fontWeight: '600',
+      },
+      title: {
+        fontSize: 32 * fontScale,
+        fontWeight: 'bold',
+        lineHeight: 32 * fontScale,
+      },
+      subtitle: {
+        fontSize: 20 * fontScale,
+        lineHeight: 20 * fontScale * 1.2,
+        fontWeight: 'bold',
+      },
+      link: {
+        lineHeight: 30 * fontScale,
+        fontSize: 16 * fontScale,
+        color: '#0a7ea4',
+      },
+    })
+  )
+};
