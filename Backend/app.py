9f72f6aced15461b736a044137d7ed52fb77db4f
Alice Sharam <as3520@cam.ac.uk>
2025-03-19 15:16:55 +0000

    Added kilo.kibtry.net to cors origins

--- Backend/app.py
+++ Backend/app.py
@@ -72,7 +72,7 @@ app = Flask(__name__)
 CORS(
     app,
     supports_credentials=True,
-    resources={r"/*": {"origins": ["http://localhost:8081", "http://127.0.0.1:8081"]}},
+    resources={r"/*": {"origins": ["https://kilo.kibtry.net", "http://localhost:8081", "http://127.0.0.1:8081"]}},
 )
 
 
